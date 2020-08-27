717a49e857eb91005e562ab2df1b15dc72388055
block_zhang <futurezhang971@163.com>
2020-08-27 16:04:12 +0800

    Stack

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,29 +7,62 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            width: 200.0,
-            height: 200.0,
-            child: Container(
-              alignment: Alignment(0.0, 1.0),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 55, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+          Stack(
+            alignment: Alignment.topLeft,
+            children: [
+              SizedBox(
+              width: 200.0,
+              height: 200.0,
+              child: Container(
+                alignment: Alignment(0.0, 1.0),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(3, 55, 255, 1.0),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
               ),
-              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
             ),
-          ),
-          SizedBox(height: 30.0,),
-          SizedBox(
-            width: 50.0,
-            height: 50.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 55, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+            SizedBox(height: 30.0,),
+            SizedBox(
+              width: 50.0,
+              height: 50.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(3, 55, 255, 1.0),
+                  // borderRadius: BorderRadius.circular(8.0),
+                  shape: BoxShape.circle,
+                  gradient: RadialGradient(
+                    colors: [
+                      Colors.red, 
+                      Colors.blue,
+                      ],
+                    ),
+                ),
+                child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
               ),
-              child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
             ),
+            Positioned(
+              child: Icon(Icons.ac_unit,color: Colors.white,size: 16.0),
+              right: 120.0,
+              top: 30.0,
+              ),
+
+              Positioned(
+              child: Icon(Icons.ac_unit,color: Colors.white,size: 16.0),
+              right: 20.0,
+              top: 12.0,
+              ),
+              Positioned(
+              child: Icon(Icons.ac_unit,color: Colors.white,size: 16.0),
+              right: 50.0,
+              top: 60.0,
+              ),
+              Positioned(
+              child: Icon(Icons.ac_unit,color: Colors.white,size: 16.0),
+              right: 80.0,
+              top: 90.0,
+              ),
+            ],
           ),
         ],
     )
