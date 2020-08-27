60199b46bbcbbfe479f090707d6c8e8e7a37d8f3
block_zhang <futurezhang971@163.com>
2020-08-27 16:18:01 +0800

    AspectRatio

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,6 +7,25 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          AspectRatio(
+            aspectRatio: 16.0 / 9.0,
+            child: Container(
+              color: Colors.yellowAccent,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StackDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
           Stack(
             alignment: Alignment.topLeft,
             children: [
