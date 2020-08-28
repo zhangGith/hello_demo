fe6c2d2db3e2e7782e2b27ea4b5e803135a0ab10
block_zhang <futurezhang971@163.com>
2020-08-28 11:11:18 +0800

    SliverAppBar

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -7,15 +7,67 @@ class SliverDemo extends StatelessWidget {
     return Scaffold(
         body: CustomScrollView(
           slivers: <Widget>[
+            SliverAppBar(
+              title: Text("你好"),
+            ),
             SliverSafeArea(
               sliver: SliverPadding(
               padding: EdgeInsets.all(8.0),
-              sliver: SliverGridDemo(),
+              sliver: SliverListDemo(),
             ),
           ),
           ],
         ),
     );
+  }
+}
+
+class SliverListDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 32.0),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(12.0),
+                      elevation: 14.0,
+                      shadowColor: Colors.grey.withOpacity(0.5),
+                      child: Stack(
+                        children: <Widget>[
+                          AspectRatio(
+                            aspectRatio: 16 / 9,
+                            child: Image.network(
+                              posts[index].imageUrl,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Positioned(
+                            top: 32.0,
+                            left: 32.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  posts[index].title,
+                                  style: TextStyle(fontStyle: FontStyle.italic, color: Colors.white, fontSize: 18.0),
+                                ),
+                                Text(
+                                  posts[index].author,
+                                  style: TextStyle(fontStyle: FontStyle.italic, color: Colors.white, fontSize: 13.0),
+                                ),
+                              ]
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                childCount: posts.length,
+              ),
+            );
   }
 }
 
