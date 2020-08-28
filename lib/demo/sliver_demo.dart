c71fe5eca3370d61cece06d3fc652031839175ac
block_zhang <futurezhang971@163.com>
2020-08-28 10:45:55 +0800

    sliver list or grid

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class SliverDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverSafeArea(
+              sliver: SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverGridDemo(),
+            ),
+          ),
+          ],
+        ),
     );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+                childAspectRatio: 1.0,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    child: Image.network(
+                      posts[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+                childCount: posts.length,
+              ),
+            );
   }
 }
