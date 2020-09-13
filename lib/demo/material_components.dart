4f7b4daa26e3b9864054585aaa70d6f81f13d3e8
block_zhang <futurezhang971@163.com>
2020-09-13 23:05:49 +0800

    BottomAppBar

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -19,12 +19,36 @@ class MaterailComponents extends StatelessWidget {
 }
 
 class FloatRaisedBtn extends StatelessWidget {
+  final Widget _floatingActionButton = FloatingActionButton(
+    onPressed: (){},
+    child: Icon(Icons.add),
+    elevation: 0.0,
+    backgroundColor: Colors.black87,
+    // shape: BeveledRectangleBorder(
+    //   borderRadius: BorderRadius.circular(30.0),
+    // ),
+  );
+
+  final Widget _floatingAddActionButton = FloatingActionButton.extended(
+    onPressed: () {}, 
+    label: Text('add'), 
+    icon: Icon(Icons.add),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('FloatRaisedBtn'),
         elevation: 0.0,
+      ),
+      floatingActionButton: _floatingActionButton,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80.0,
+        ),
+        shape: CircularNotchedRectangle(),
       ),
     );
   }
