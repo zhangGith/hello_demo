fe553d60156d7d4ad47e66ee09ae7f2773cccc49
block_zhang <futurezhang971@163.com>
2020-09-13 21:20:29 +0800

    SnackBar：在屏幕底部动画弹出提示栏

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -32,10 +32,35 @@ class _RegisterDemoState extends State<RegisterDemo> {
   final _registerFormKey = GlobalKey<FormState>();
   String username, password;
 
+  bool _autovalidate = false;
+
   void _submitRegisterForm() {
-    _registerFormKey.currentState.save();
-    debugPrint('username: $username');
-    debugPrint('password: $password');
+
+    if (_registerFormKey.currentState.validate()) {
+      _registerFormKey.currentState.save();
+
+      debugPrint('username: $username');
+      debugPrint('password: $password');
+    } else {
+      setState(() {
+        _autovalidate = true;
+      });
+    }
+    
+  }
+
+  String validateUsername(value) {
+    if (value.isEmpty) {
+        return 'name is empty';
+    }
+    return null;
+  }
+
+  String validatePasssword(value) {
+      if (value.isEmpty) {
+          return 'password is empty';
+      }
+      return null;
   }
 
   @override
@@ -48,19 +73,26 @@ class _RegisterDemoState extends State<RegisterDemo> {
           TextFormField(
             decoration: InputDecoration(
               labelText: 'username',
+              helperText: '',
             ),
             onSaved: (value) {
               username = value;
             },
+            validator: validateUsername,
+            autovalidate: _autovalidate,
           ),
            TextFormField(
              obscureText: true, //密文
             decoration: InputDecoration(
               labelText: 'password',
+              helperText: '',
+
             ),
             onSaved: (value) {
               password = value;
             },
+            validator: validatePasssword,
+            autovalidate: _autovalidate,
           ),
           SizedBox(height: 32.0),
           Container(
