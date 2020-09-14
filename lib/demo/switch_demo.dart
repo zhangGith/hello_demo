44820e96a1aa36e6a3fb7362643cc7c99a921f94
block_zhang <futurezhang971@163.com>
2020-09-14 23:00:12 +0800

    switch title

--- lib/demo/switch_demo.dart
+++ lib/demo/switch_demo.dart
@@ -21,18 +21,30 @@ class _SwitchDmeoState extends State<SwitchDmeo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SwitchListTile(
+              value: _switchValue, 
+              onChanged: (value){
+                setState(() {
+                  _switchValue = value;
+                });
+              },
+              title: Text('A'),
+              subtitle: Text('A sub'),
+              secondary: Icon(_switchValue ? Icons.inbox : Icons.info),
+              selected: _switchValue,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                  Text(_switchValue ? '😄' : '😔', style: TextStyle(fontSize: 32.0)),
-                  Switch(
-                    value: _switchValue, 
-                    onChanged: (value) {
-                      setState(() {
-                        _switchValue = value;
-                      });
-                    }
-                  ),
+                  // Text(_switchValue ? '😄' : '😔', style: TextStyle(fontSize: 32.0)),
+                  // Switch(
+                  //   value: _switchValue, 
+                  //   onChanged: (value) {
+                  //     setState(() {
+                  //       _switchValue = value;
+                  //     });
+                  //   }
+                  // ),
               ],
             ),
           ],
