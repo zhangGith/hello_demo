e983d3535332236b52f73dbcfe43a54e019b50f0
block_zhang <futurezhang971@163.com>
2020-09-14 21:54:33 +0800

    创建用户输入相关的小部件的演示页面

---
+++ lib/demo/check_box_demo.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CheckBoxDemo extends StatefulWidget {
+  @override
+  _CheckBoxDemoState createState() => _CheckBoxDemoState();
+}
+
+class _CheckBoxDemoState extends State<CheckBoxDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('_WidgetDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
