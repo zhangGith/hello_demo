03813c93f42022991d39f396979e7991327bdb09
block_zhang <futurezhang971@163.com>
2020-09-16 22:40:10 +0800

    SimpleDialog：获取与使用选项的值

--- lib/demo/simple_dialog_demo.dart
+++ lib/demo/simple_dialog_demo.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+enum Option {
+  A, B, C
+}
 
 class SimpleDialogDemo extends StatefulWidget {
   @override
@@ -7,8 +12,9 @@ class SimpleDialogDemo extends StatefulWidget {
 
 class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
 
-  _openSimleDialog() {
-    showDialog(
+  String _choiceStr = 'not thing';
+  Future _openSimleDialog() async {
+    final option = await showDialog(
       context: context,
       builder: (context) {
         return SimpleDialog(
@@ -17,25 +23,43 @@ class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
             SimpleDialogOption(
               child: Text('a'),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context, Option.A);
               },
             ),
             SimpleDialogOption(
               child: Text('a'),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context, Option.B);
               },
             ),
             SimpleDialogOption(
               child: Text('a'),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context, Option.C);
               },
             ),
           ],
         );
       },
     );
+    switch (option) {
+      case Option.A:
+        setState(() {
+          _choiceStr = 'A';
+        });
+        break;
+      case Option.B:
+        setState(() {
+          _choiceStr = 'B';
+        });
+        break;
+      case Option.C:
+        setState(() {
+          _choiceStr = 'C';
+        });
+        break;
+      default:
+    }
   }
 
   @override
@@ -50,6 +74,7 @@ class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('your choice is ${_choiceStr}'),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
