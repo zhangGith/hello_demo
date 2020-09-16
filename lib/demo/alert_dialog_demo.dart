76e095521adb9b126be8db5c227aacde6622d6fa
block_zhang <futurezhang971@163.com>
2020-09-16 23:06:30 +0800

    alert dialog value

--- lib/demo/alert_dialog_demo.dart
+++ lib/demo/alert_dialog_demo.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+enum Action {
+  OK,
+  Cancel
+}
 
 class AlertDialogDemo extends StatefulWidget {
   @override
@@ -7,8 +13,9 @@ class AlertDialogDemo extends StatefulWidget {
 
 class _AlertDialogDemoState extends State<AlertDialogDemo> {
 
-  _openAlertDialog() {
-    showDialog(
+  String _actionTitle = 'no thing';
+  Future _openAlertDialog() async {
+    Action action = await showDialog(
       barrierDismissible: false,
       context: context,
       builder: (BuildContext context) {
@@ -16,12 +23,26 @@ class _AlertDialogDemoState extends State<AlertDialogDemo> {
           title: Text('alert dialog'.toUpperCase()),
           content: Text('subtitle'),
           actions: [
-            FlatButton(onPressed: () { Navigator.pop(context); }, child: Text('cancel')),
-            FlatButton(onPressed: () { Navigator.of(context); }, child: Text('ok')),
+            FlatButton(onPressed: () { Navigator.pop(context, Action.Cancel); }, child: Text('cancel')),
+            FlatButton(onPressed: () { Navigator.pop(context, Action.OK); }, child: Text('ok')),
           ],
         );
       },
     );
+
+    switch (action) {
+      case Action.OK:
+        setState (() {
+          _actionTitle = 'ok';
+        });
+        break;
+      case Action.Cancel:
+        setState (() {
+          _actionTitle = 'cancel';
+        });
+        break;
+      default:
+    }
   }
 
   @override
@@ -36,6 +57,8 @@ class _AlertDialogDemoState extends State<AlertDialogDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('you choice is $_actionTitle'),
+            SizedBox(height: 32.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
