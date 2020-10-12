24baf2d27681351bb9a81b81f435b8f4fec15f37
block_zhang <futurezhang971@163.com>
2020-10-12 23:37:58 +0800

    创建与使用 InheritedWidget 有效地传递数据给子部件

--- lib/demo/state/state_management.dart
+++ lib/demo/state/state_management.dart
@@ -17,45 +17,65 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return CounterProvider(
+      count: _count,
+      increaseCount: _increaseCount,
+      child: Scaffold(
       appBar: AppBar(
-        title: Text('StateManagementDemo'),
-        elevation: 0.0,
-      ),
-      body: CounterWrapper(_count, _increaseCount),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _increaseCount,
-        child: Icon(Icons.add),
-      ),
+          title: Text('StateManagementDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterWrapper(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _increaseCount,
+          child: Icon(Icons.add),
+        ),
+      )
     );
   }
 }
 
 class CounterWrapper extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-  CounterWrapper(this.count, this.increaseCount);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CounterDemo(count, increaseCount),
+      child: CounterDemo(),
     );
   }
 }
 
 class CounterDemo extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-  CounterDemo(this.count, this.increaseCount);
 
   @override
   Widget build(BuildContext context) {
+    final int count = CounterProvider.of(context).count;
+    final VoidCallback increaseCount = CounterProvider.of(context).increaseCount;
     return ActionChip(
           label: Text('$count'),
           onPressed: increaseCount,
         );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final int count;
+  final VoidCallback increaseCount;
+  final Widget child;
+
+  CounterProvider({
+    this.count,
+    this.increaseCount,
+    this.child,
+  }) : super(child: child);
+
+  // static CounterProvider of(BuildContext context) => context.inheritFromWidgetOfExactType(CounterProvider);
+  // dependOnInheritedWidgetOfExactType
+  static CounterProvider of(BuildContext context) => context.dependOnInheritedWidgetOfExactType(aspect: CounterProvider);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    return true;
   }
 }
