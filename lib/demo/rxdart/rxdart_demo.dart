f0e752918a6015f1fe68e38866a9792ed3215270
block_zhang <futurezhang971@163.com>
2020-10-15 17:00:19 +0800

    使用 Subjects 控制 Observables

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -22,18 +22,24 @@ class RxDartDemoHome extends StatefulWidget {
 
 class _RxDartDemoHomeState extends State<RxDartDemoHome> {
   @override
-  void initState() {
+  Future<void> initState() async {
     // TODO: implement initState
     super.initState();
 
-    Observable<String> _observable =
-        // Observable(Stream.fromIterable(['hello ~', '您好']));
-        // Observable.fromFuture(Future.value('hello~'));
-        // Observable.fromIterable(['hello~', '你好']);
-        // Observable.just('hello~');
-        Observable.periodic(Duration(seconds: 3), (x) => x.toString());
-    _observable.listen(print);
-  }
+    // Observable<String> _observable =
+    //     // Observable(Stream.fromIterable(['hello ~', '您好']));
+    //     // Observable.fromFuture(Future.value('hello~'));
+    //     // Observable.fromIterable(['hello~', '你好']);
+    //     // Observable.just('hello~');
+    //     Observable.periodic(Duration(seconds: 3), (x) => x.toString());
+    // _observable.listen(print);
+
+    PublishSubject<String> _subject = PublishSubject<String>();
+    _subject.listen(data) => print('listen 1 : $data');
+    _subject.add('hello');
+    _subject.listen((data) => print('listen 2 : $data');
+    _subject.add('hola');
+;  }
 
   @override
   Widget build(BuildContext context) {
