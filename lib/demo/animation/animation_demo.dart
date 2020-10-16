c09193e9d9697ff46eae91d84e2479e1d3cb902e
block_zhang <futurezhang971@163.com>
2020-10-16 17:04:50 +0800

    Curve：动画曲线

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -23,6 +23,7 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
   AnimationController animationDemoController;
   Animation animation;
   Animation animationColor;
+  CurvedAnimation curve;
 
   @override
   void initState() {
@@ -36,10 +37,11 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
       // upperBound: 100.0,
       // value: 32.0,
     );
-
-    animation = Tween(begin: 32.0, end: 100.0).animate(animationDemoController);
-    animationColor = ColorTween(begin: Colors.red, end: Colors.red[900])
-        .animate(animationDemoController);
+    curve = CurvedAnimation(
+        parent: animationDemoController, curve: Curves.bounceInOut);
+    animation = Tween(begin: 32.0, end: 100.0).animate(curve);
+    animationColor =
+        ColorTween(begin: Colors.red, end: Colors.red[900]).animate(curve);
 
     animationDemoController.addListener(() {
       // print('${animationDemoController.value}');
