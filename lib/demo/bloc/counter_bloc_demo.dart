78747fa4af5ef7d5dd03b3236a24ab53f70cff32
block_zhang <futurezhang971@163.com>
2020-10-16 10:51:45 +0800

    使用 InheritedWidget 传递 BLoC

--- lib/demo/bloc/counter_bloc_demo.dart
+++ lib/demo/bloc/counter_bloc_demo.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class CounterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
+
     return Center(
       child: ActionChip(
         label: Text('0'),
-        onPressed: () {},
+        onPressed: () {
+          _counterBloc.log();
+        },
       ),
     );
   }
@@ -15,9 +19,40 @@ class CounterHome extends StatelessWidget {
 class CounterActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
+
     return FloatingActionButton(
       child: Icon(Icons.add),
-      onPressed: () {},
+      onPressed: () {
+        print('object');
+        _counterBloc.log();
+      },
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final Widget child;
+  final CounterBloc bloc;
+
+  CounterProvider({
+    this.child,
+    this.bloc,
+  }) : super(child: child);
+
+  static CounterProvider of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType();
+  // context.inheritFromWidgetOfExactType(CounterProvider);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    return true;
+  }
+}
+
+class CounterBloc {
+  void log() {
+    print('bloc');
   }
 }
